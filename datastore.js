0d13b4702f500d574fba2472a452e738c5dc195d
Yatendrajain <yatendrawebdesigner@gmail.com>
2020-12-29 21:30:41 +0530

    Impleted error handling in create, read and delete method

--- datastore.js
+++ datastore.js
@@ -7,38 +7,76 @@ var LocalStorage = require("node-localstorage").LocalStorage;
 localStorage = new LocalStorage("./scratch");
 
 //Class DataStore
-
 var dataStore = class dataStore {
+    //Constructor
+    constructor() {
+        this.key = "";
+        this.value = null;
+        this.time_to_live = 0;
+        this.valueobj = { value: this.value, time_to_live: this.time_to_live }; //Value as json object
+        this.jsonobj = { key: this.value }; //Initializing JSON Object
+    }
+
     //Create Method
     /**
      * Create Key-Value Pair In Local Storage.
-     * @param {any} key - To set a key for some value
+     * @param {string} key - To set a key for some value
      * @param {any} value - To set a value for a specific key
      * @param {int} time_to_live - Maximum time to read or delete the key-value pair. Default = 0
      */
     create(key, value, time_to_live = 0) {
-        localStorage.setItem(key, value);
-        console.log("Success");
+        //Variable Declaration
+        this.valueobj = { value: value, time_to_live: time_to_live }; //Updating Json Object to append
+        var size = Buffer.byteLength(JSON.stringify(this.valueobj)); //Calculating the size of json object
+        var sizeInKB = size / 1024; //Calculating the size in KB
+
+        // Error Handling
+        if (key.length > 32) {
+            console.log("Key is greater than 32 character long.");
+        } else if (sizeInKB > 16) {
+            console.log("Size of json object exceeds 16KB");
+        } else if (JSON.parse(localStorage.getItem(key)) != null) {
+            console.log("Key already Exist in local Storage");
+        } else {
+            localStorage.setItem(key, JSON.stringify(this.valueobj));
+            console.log(
+                "Success! Key: " +
+                key +
+                ", value: " +
+                value +
+                " is added" +
+                "\nTime to live: " +
+                time_to_live,
+            );
+        }
     }
 
     //Read Method
     /**
-     * Read Key-Value Pair In Local Storage.
-     * @param {any} key - To read key-value pair of specific key
+     * Read and returns Key-Value Pair In Local Storage.
+     * @param {string} key - To read key-value pair of specific key
      */
     read(key) {
-        var temp = localStorage.getItem(key);
-        console.log(temp);
+        this.value = localStorage.getItem(key);
+        return this.value;
     }
 
     //Delete Method
     /**
      * Delete Key-Value Pair In Local Storage.
-     * @param {any} key - To delete key-value pair of specific key
+     * @param {string} key - To delete key-value pair of specific key
      */
     delete(key) {
-        localStorage.removeItem(key);
-        console.log("Success");
+        //Error Handling
+        if (localStorage.getItem(key) == null) {
+            console.log("Key doesn't exist in local storage");
+        } else {
+            var deleted_value = localStorage.getItem(key);
+            localStorage.removeItem(key);
+            console.log(
+                "Success! Key: " + key + ", value: " + deleted_value + " is deleted",
+            );
+        }
     }
 };
 
--- example.js
+++ example.js
@@ -1,4 +1,6 @@
 const dataStore = require("./datastore");
 
 const store = new dataStore();
-store.create("yoh", "bro");
+store.create("Yatendra", 20000, 10);
+// console.log(store.read("Yatendra"));
+// store.delete("Yatendra");
